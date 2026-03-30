ca1919265a0e330ab21ea48064c29e9dab62f37e
Victor Fattor <victor.fattor@bosonic.digital>
2026-03-30 01:09:16 -0300

    second server  - file

---
+++ hello_html.js
@@ -0,0 +1,10 @@
+var http = require("http"),
+    fs = require("fs");
+
+
+var html = fs.readFileSync("./index.html");
+
+http.createServer(function(request, response){
+  response.write(html);
+  response.end();
+}).listen(8080);
